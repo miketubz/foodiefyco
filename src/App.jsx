7f5a1738c966c220f801844f6eb5aa024bda82b1
Micky Tubz <mickytubie@gmail.com>
2026-04-15 00:47:42 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,58 +1,48 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { AdminPanel } from './components/AdminPanel.jsx';
-import MenuAdminPanel from './components/MenuAdminPanel.jsx';
-import RequireAdmin from './components/RequireAdmin.jsx';
-import AdminLoginPage from './pages/AdminLoginPage.jsx';
-import FrontendPage from './pages/FrontendPage.jsx';
-import GalleryPage from './pages/GalleryPage.jsx';
-import AdminGalleryPage from './pages/AdminGalleryPage.jsx';
-import AdminExternalPage from './pages/AdminExternalPage.jsx';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import FrontendPage from './pages/FrontendPage';
+import AdminPanel from './pages/AdminPanel';
+import MenuAdminPanel from './pages/MenuAdminPanel';
+import AdminLoginPage from './pages/AdminLoginPage';
+import RequireAdmin from './components/RequireAdmin';
+import AdminExternalPage from './pages/AdminExternalPage';
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<FrontendPage />} />
-      <Route path="/gallery" element={<GalleryPage />} />
-      <Route path="/admin/login" element={<AdminLoginPage />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<FrontendPage />} />
+        <Route path="/admin/login" element={<AdminLoginPage />} />
 
-      <Route
-        path="/admin"
-        element={
-          <RequireAdmin>
-            <AdminPanel />
-          </RequireAdmin>
-        }
-      />
+        <Route
+          path="/admin"
+          element={
+            <RequireAdmin>
+              <AdminPanel />
+            </RequireAdmin>
+          }
+        />
 
-      <Route
-        path="/admin/menu"
-        element={
-          <RequireAdmin>
-            <MenuAdminPanel />
-          </RequireAdmin>
-        }
-      />
+        <Route
+          path="/admin/menu"
+          element={
+            <RequireAdmin>
+              <MenuAdminPanel />
+            </RequireAdmin>
+          }
+        />
 
-      <Route
-        path="/admin/gallery"
-        element={
-          <RequireAdmin>
-            <AdminGalleryPage />
-          </RequireAdmin>
-        }
-      />
+        <Route
+          path="/admin/external"
+          element={
+            <RequireAdmin>
+              <AdminExternalPage />
+            </RequireAdmin>
+          }
+        />
 
-      <Route
-        path="/admin/external"
-        element={
-          <RequireAdmin>
-            <AdminExternalPage />
-          </RequireAdmin>
-        }
-      />
-
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
