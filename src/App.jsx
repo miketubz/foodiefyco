d42e5d5c72b6cb921ac594520d3673f817fd7b87
Micky Tubz <mickytubie@gmail.com>
2026-04-17 02:20:19 +0800

    Redirect all routes to /admin and remove LoginPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,20 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import AdminPanel from './pages/AdminPanel';
 import ArchivePage from './pages/ArchivePage';
 import ProfitCalculator from './pages/ProfitCalculator';
-import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Login Route */}
-        <Route path="/admin/login" element={<LoginPage />} />
-        
         {/* Admin Routes */}
         <Route path="/admin" element={<AdminPanel />} />
         <Route path="/admin/archive" element={<ArchivePage />} />
         <Route path="/admin/profit" element={<ProfitCalculator />} />
         
-        {/* Redirects - since you don't have a public home page */}
-        <Route path="/" element={<Navigate to="/admin/login" replace />} />
-        <Route path="*" element={<Navigate to="/admin/login" replace />} />
+        {/* Redirect everything else to /admin */}
+        <Route path="/" element={<Navigate to="/admin" replace />} />
+        <Route path="/admin/login" element={<Navigate to="/admin" replace />} />
+        <Route path="*" element={<Navigate to="/admin" replace />} />
       </Routes>
     </Router>
   );
