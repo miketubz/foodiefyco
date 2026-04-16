61628da567e3c1d3934ee62fbeec8cbfa1c8409a
Micky Tubz <mickytubie@gmail.com>
2026-04-16 17:53:26 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,21 @@
-import { Routes, Route, Navigate } from 'react-router-dom';
-import { AdminPanel } from './components/AdminPanel.jsx';
-import { AdminPanel2 } from './components/AdminPanel2.jsx';
-import MenuAdminPanel from './components/MenuAdminPanel.jsx';
-import RequireAdmin from './components/RequireAdmin.jsx';
-import AdminLoginPage from './pages/AdminLoginPage.jsx';
-import FrontendPage from './pages/FrontendPage.jsx';
-import GalleryPage from './pages/GalleryPage.jsx';
-import AdminGalleryPage from './pages/AdminGalleryPage.jsx';
-import AdminExternalPage from './pages/AdminExternalPage.jsx';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import AdminPanel from './pages/AdminPanel';
+import ArchivePage from './pages/ArchivePage';
+import ProfitCalculator from './pages/ProfitCalculator';
+// Import your other existing pages here
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<FrontendPage />} />
-      <Route path="/gallery" element={<GalleryPage />} />
-      <Route path="/admin/login" element={<AdminLoginPage />} />
-
-      <Route
-        path="/admin"
-        element={
-          <RequireAdmin>
-            <AdminPanel />
-          </RequireAdmin>
-        }
-      />
-
-      <Route
-        path="/admin2"
-        element={
-          <RequireAdmin>
-            <AdminPanel2 />
-          </RequireAdmin>
-        }
-      />
-
-      <Route
-        path="/admin/menu"
-        element={
-          <RequireAdmin>
-            <MenuAdminPanel />
-          </RequireAdmin>
-        }
-      />
-
-      <Route
-        path="/admin/gallery"
-        element={
-          <RequireAdmin>
-            <AdminGalleryPage />
-          </RequireAdmin>
-        }
-      />
-
-      <Route
-        path="/admin/external"
-        element={
-          <RequireAdmin>
-            <AdminExternalPage />
-          </RequireAdmin>
-        }
-      />
-
-      <Route path="*" element={<Navigate to="/" replace />} />
-    </Routes>
+    <Router>
+      <Routes>
+        {/* Your existing routes */}
+        <Route path="/admin" element={<AdminPanel />} />
+        <Route path="/admin/archive" element={<ArchivePage />} />
+        <Route path="/admin/profit" element={<ProfitCalculator />} />
+        {/* Add other routes */}
+      </Routes>
+    </Router>
   );
 }
 
