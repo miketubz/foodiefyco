e7d0d8de27baa9214e0d4350d22063fd0a96ef2a
Micky Tubz <mickytubie@gmail.com>
2026-04-17 04:21:48 +0800

    Add SellerPanel route to App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdminPanel from './pages/AdminPanel';
+ import SellerPanel from './pages/SellerPanel'; // top
+   
+   <Route path="/sellerpanel" element={<SellerPanel />} /> // inside <Routes>
 
 function App() {
   return (
@@ -15,3 +18,5 @@ function App() {
 }
 
 export default App;
+
+  
