463f368666980a1b8f9546cd6f24be9e15c3b9dd
Micky Tubz <mickytubie@gmail.com>
2026-04-17 05:19:12 +0800

    Remove SellerPanel route from App component
    Removed SellerPanel route from the application.

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdminPanel from './pages/AdminPanel';
-import SellerPanel from './pages/SellerPanel';
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/sellerpanel" element={<SellerPanel />} />
-        <Route path="/" element={<Navigate to="/admin" replace />} />
+                <Route path="/" element={<Navigate to="/admin" replace />} />
         <Route path="*" element={<Navigate to="/admin" replace />} />
       </Routes>
     </Router>
