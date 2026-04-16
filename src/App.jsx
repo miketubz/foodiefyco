fb694f617bd4256299fd4b8025fd14cdd42f8826
Micky Tubz <mickytubie@gmail.com>
2026-04-17 03:14:58 +0800

    Remove ArchivePage and ProfitCalculator routes
    Removed unused routes for ArchivePage and ProfitCalculator.

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import AdminPanel from './pages/AdminPanel';
-import ArchivePage from './pages/ArchivePage';
-import ProfitCalculator from './pages/ProfitCalculator';
 
 function App() {
   return (
     <Router>
       <Routes>
-        {/* Admin Routes */}
         <Route path="/admin" element={<AdminPanel />} />
-        <Route path="/admin/archive" element={<ArchivePage />} />
-        <Route path="/admin/profit" element={<ProfitCalculator />} />
-        
-        {/* Redirect everything else to /admin */}
         <Route path="/" element={<Navigate to="/admin" replace />} />
-        <Route path="/admin/login" element={<Navigate to="/admin" replace />} />
         <Route path="*" element={<Navigate to="/admin" replace />} />
       </Routes>
     </Router>
